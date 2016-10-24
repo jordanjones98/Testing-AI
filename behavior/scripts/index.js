de2d38db512d38ceab8d39a1b854c462fb9897df
Jordan Jones <jordanalanjones@gmail.com>
2016-10-24 14:16:58 -0500

    Using known good code to fix errors.

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,61 +1,117 @@
+// 'use strict'
+//
+// exports.handle = function handle(client) {
+//
+//   const sayHello = client.createStep({
+//     satisfied() {
+//       return Boolean(client.getConversationState().helloSent)
+//     },
+//
+//     prompt() {
+//       client.addResponse('app:response:name:welcome')
+//       client.addResponse('app:response:name:provide/documentation', {
+//         documentation_link: 'http://docs.init.ai',
+//       })
+//       client.addResponse('app:response:name:provide/instructions')
+//       client.updateConversationState({
+//         helloSent: true
+//       })
+//       client.done()
+//     }
+//   })
+//
+//   // Steps
+//   const handleGreeting = client.createStep({
+//       satisfied() {
+//           return false
+//       },
+//
+//       prompt() {
+//           client.addTextResponse('Hello world, I mean human')
+//           client.done()
+//       }
+//   })
+//
+//   const handleGoodbye = client.createStep({
+//       satisfied() {
+//           return false
+//       },
+//
+//       prompt() {
+//           client.addTextResponse('See you later!')
+//           client.done
+//       }
+//   })
+//
+//   const whatIsHackCes = client.createStep({
+//       satisfied() {
+//           return false
+//       },
+//
+//       prompt() {
+//           client.addTextResponse('Hello! Hack CES is ____')
+//           client.done
+//       }
+//   })
+//
+//   const getEmail = client.createStep({
+//       satisfied() {
+//           return false
+//       },
+//
+//       prompt() {
+//           client.addTextResponse('We are excited that you are interested! Could we please get your email to follow up?')
+//           client.done
+//       }
+//   })
+//
+//   // Flow
+//   client.runFlow({
+//     classifications: {
+//         greeting: 'greeting',
+//     },
+//     autoResponses: {
+//       // configure responses to be automatically sent as predicted by the machine learning model
+//     },
+//     streams: {
+//       greeting: whatIsHackCes,
+//       goodbye: handleGoodbye,
+//       main: 'onboarding',
+//       onboarding: [sayHello],
+//     }
+//   })
+// }
+
+
 'use strict'
 
 exports.handle = function handle(client) {
-
   const sayHello = client.createStep({
     satisfied() {
-      return Boolean(client.getConversationState().helloSent)
+      return false
     },
 
     prompt() {
-      client.addResponse('app:response:name:welcome')
-      client.addResponse('app:response:name:provide/documentation', {
-        documentation_link: 'http://docs.init.ai',
-      })
-      client.addResponse('app:response:name:provide/instructions')
-      client.updateConversationState({
-        helloSent: true
-      })
+      client.addResponse('app:response:name:hello')
       client.done()
     }
   })
 
-  // Steps
+  const handleEvent = function(eventType, payload) {
+    console.log('received event of type', eventType, 'with payload:', payload)
+    client.addTextResponse('Received event of type: ' + eventType)
+    client.done()
+  }
 
-  const whatIsHackCes = client.createStep({
-      satisfied() {
-          return false
-      },
+  console.log('Received message:', client.getMessagePart())
 
-      prompt() {
-          client.addTextResponse('Hello! Hack CES is ____')
-          client.done
-      }
-  })
-
-  const getEmail = client.createStep({
-      satisfied() {
-          return false
-      },
-
-      prompt() {
-          client.addTextResponse('We are excited that you are interested! Could we please get your email to follow up?')
-          client.done
-      }
-  })
-
-  // Flow
   client.runFlow({
-    classifications: {
-        greeting: 'greeting'
-    },
-    autoResponses: {
-      // configure responses to be automatically sent as predicted by the machine learning model
+    eventHandlers: {
+      '*': handleEvent
     },
     streams: {
-      greeting: ['whatIsHackCes', 'getEmail'],
-      main: 'onboarding',
-      onboarding: [sayHello, whatIsHackCes]
+      main: 'hi',
+      hi: [sayHello],
     }
   })
 }
