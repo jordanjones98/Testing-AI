c8c3ec16d92a3ea47efc1b7765aead50e0997ce1
Jordan Jones <jordanalanjones@gmail.com>
2016-10-25 11:39:52 -0500

    Nesting Yes/No

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -2,11 +2,64 @@
 
 exports.handle = function handle(client) {
 const showStuffCES = client.createStep({
+
+    satisfied() {
+        return (typeof client.getConversationState().willingToMeet !== 'undefined')
+    },
+
+    next() {
+      const willingToMeet = client.getConversationState().willingToMeet
+      if (willingToMeet === true) {
+          return 'willingToMeet'
+      } else if (willingToMeet === false) {
+          return 'notWillingToMeet'
+      }
+  },
+
+    prompt() {
+        let baseClassificationMeet = client.getMessagePart().classification.base_type.value
+        if (baseClassificationMeet === 'affirmative') {
+          client.updateConversationState({
+            willingToMeet: true,
+          })
+          return 'init.proceed' // `next` from this step will get called
+      } else if (baseClassificationMeet === 'decline') {
+          client.updateConversationState({
+            willingToMeet: false,
+          })
+          return 'init.proceed' // `next` from this step will get called
+        }
+
+        client.addTextResponse('Are you looking to meet people there?')
+
+        // If the next message is a 'decline', like 'don't know'
+        // or an 'affirmative', like 'yeah', or 'that's right'
+        // then the current stream will be returned to
+        client.expect(client.getStreamName(), ['affirmative', 'decline'])
+        client.done()
+    },
+
+
+})
+
+const willingToMeetMessage = client.createStep({
     satisfied() {
         return false
     },
+
     prompt() {
-        client.addTextResponse('Awesome!')
+        client.addTextResponse('Thats great, who are you looking to meet?')
+        client.done()
+    }
+})
+
+const notWillingToMeetMessage = client.createStep({
+    satisfied() {
+        return false
+    },
+
+    prompt() {
+        client.addTextResponse('Hmm, we wish you were. I think we could help you meet someone.')
         client.done()
     }
 })
@@ -16,7 +69,7 @@ const noShowStuffCES = client.createStep({
         return false
     },
     prompt() {
-        client.addTextResponse('Thats too bad..')
+        client.addTextResponse('Thats too bad.. If you would like to learn more about it check it out here! https://www.ces.tech/')
         client.done()
     }
 })
@@ -65,97 +118,10 @@ client.runFlow({
     showContent: [checkIfGoingToCES],
     showCES: [showStuffCES],
     noShowCES: [noShowStuffCES],
+    willingToMeet: [willingToMeetMessage],
+    notWillingToMeet: [notWillingToMeetMessage],
+
   }
 })
 
 }
-//
-    // WORKING CODE UNDER HERE
-//
-// 'use strict'
-//
-// exports.handle = function handle(client) {
-//   // const sayHello = client.createStep({
-//   //   satisfied() {
-//   //     return Boolean(client.getConversationState().helloSent)
-//   //   },
-//   //
-//   //   prompt() {
-//   //     client.addResponse('app:response:name:welcome')
-//   //     client.addResponse('app:response:name:provide/documentation', {
-//   //       documentation_link: 'http://docs.init.ai',
-//   //     })
-//   //     client.addResponse('app:response:name:provide/instructions')
-//   //     client.updateConversationState({
-//   //       helloSent: true
-//   //     })
-//   //     client.done()
-//   //   }
-//   // })
-//
-//   // const untrained = client.createStep({
-//   //   satisfied() {
-//   //     return false
-//   //   },
-//   //
-//   //   prompt() {
-//   //     client.addResponse('app:response:name:apology/untrained')
-//   //     client.done()
-//   //   }
-//   // })
-//
-//   const handleGreeting = client.createStep({
-//     satisfied() {
-//       return false
-//     },
-//
-//     prompt() {
-//       client.addTextResponse('Hello, and welcome to Hack CES!')
-//       client.addTextResponse('Are you going to CES this January?')
-//       client.done()
-//     }
-//   })
-//
-//   const handleGoodbye = client.createStep({
-//     satisfied() {
-//       return false
-//     },
-//
-//     prompt() {
-//       client.addTextResponse('See you later!')
-//       client.done()
-//     }
-//   })
-//
-//   // Yes No
-//   const afirmativeGoing = client.createStep({
-//       satisfied() {
-//           return false
-//       },
-//
-//       prompt() {
-//           client.addTextResponse('Awesome! We\'re glad you\'re going!')
-//       }
-//   })
-//
-//   const negativeGoing = client.createStep({
-//       satisfied() {
-//           return false
-//       },
-//
-//       prompt() {
-//           client.addTextResponse('That\'s to bad. You can check it out by clicking this link! https://www.ces.tech/')
-//       }
-//   })
-//
-//   client.runFlow({
-//     classifications: {
-//       goodbye: 'goodbye',
-//       greeting: 'greeting',
-//     },
-//     streams: {
-//       goodbye: handleGoodbye,
-//       greeting: handleGreeting,
-//     }
-//   })
-// }
