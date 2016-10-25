2f7a4f65b3efbf4b2ad627208acd479371814f82
Jordan Jones <jordanalanjones@gmail.com>
2016-10-24 19:27:39 -0500

    Yes/No

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,88 +1,3 @@
-// 'use strict'
-//
-// exports.handle = function handle(client) {
-//
-//   const sayHello = client.createStep({
-//     satisfied() {
-//       return Boolean(client.getConversationState().helloSent)
-//     },
-//
-//     prompt() {
-//       client.addResponse('app:response:name:welcome')
-//       client.addResponse('app:response:name:provide/documentation', {
-//         documentation_link: 'http://docs.init.ai',
-//       })
-//       client.addResponse('app:response:name:provide/instructions')
-//       client.updateConversationState({
-//         helloSent: true
-//       })
-//       client.done()
-//     }
-//   })
-//
-//   // Steps
-//   const handleGreeting = client.createStep({
-//       satisfied() {
-//           return false
-//       },
-//
-//       prompt() {
-//           client.addTextResponse('Hello world, I mean human')
-//           client.done()
-//       }
-//   })
-//
-//   const handleGoodbye = client.createStep({
-//       satisfied() {
-//           return false
-//       },
-//
-//       prompt() {
-//           client.addTextResponse('See you later!')
-//           client.done
-//       }
-//   })
-//
-//   const whatIsHackCes = client.createStep({
-//       satisfied() {
-//           return false
-//       },
-//
-//       prompt() {
-//           client.addTextResponse('Hello! Hack CES is ____')
-//           client.done
-//       }
-//   })
-//
-//   const getEmail = client.createStep({
-//       satisfied() {
-//           return false
-//       },
-//
-//       prompt() {
-//           client.addTextResponse('We are excited that you are interested! Could we please get your email to follow up?')
-//           client.done
-//       }
-//   })
-//
-//   // Flow
-//   client.runFlow({
-//     classifications: {
-//         greeting: 'greeting',
-//     },
-//     autoResponses: {
-//       // configure responses to be automatically sent as predicted by the machine learning model
-//     },
-//     streams: {
-//       greeting: whatIsHackCes,
-//       goodbye: handleGoodbye,
-//       main: 'onboarding',
-//       onboarding: [sayHello],
-//     }
-//   })
-// }
-
-
 'use strict'
 
 exports.handle = function handle(client) {
@@ -97,21 +12,57 @@ exports.handle = function handle(client) {
       }
   })
 
-  const handleEvent = function(eventType, payload) {
-    console.log('received event of type', eventType, 'with payload:', payload)
-    client.addTextResponse('Received event of type: ' + eventType)
-    client.done()
+  const goingToCES = client.createStep({
+      client.addTextResponse('Great! You\'re going to CES!')
+  })
+
+  const notGoingToCES = client.createStep({
+      client.addTextResponse('Hmm... You\'re not going.. Check out out here!')
+  })
+
+  const checkIfAfirmative = client.createStep({
+    satisfied() {
+      return (typeof client.getConversationState().isAfirmative !== 'undefined')
+    },
+
+    next() {
+      const isOverEighteen = client.getConversationState().isAfirmative
+      if (isAfirmative === true) {
+          return 'afirmativeGoing'
+      } else if (isAfirmative === false) {
+          return 'afirmativeNotGoing'
+      }
+    }
+    prompt() {
+  let baseClassification = client.getMessagePart().classification.base_type.value
+  if (baseClassification === 'affirmative') {
+    client.updateConversationState({
+      checkIfAfirmative: true,
+    })
+    return 'init.proceed' // `next` from this step will get called
+  } else if (baseClassification === 'decline') {
+    client.updateConversationState({
+      checkIfAfirmative: false,
+    })
+    return 'init.proceed' // `next` from this step will get called
   }
 
-  console.log('Received message:', client.getMessagePart())
+  client.addResponse('app:response:name:ask_over_eighteen')
 
-  client.runFlow({
-    eventHandlers: {
-      '*': handleEvent
-    },
-    streams: {
-      main: 'hi',
-      hi: [sayHello],
-    }
-  })
+  // If the next message is a 'decline', like 'don't know'
+  // or an 'affirmative', like 'yeah', or 'that's right'
+  // then the current stream will be returned to
+  client.expect(client.getStreamName(), ['affirmative', 'decline'])
+  client.done()
+}
+})
+
+client.runFlow({
+  streams: {
+    main: 'showContent',
+    showContent: [checkIfAfirmative],
+    afirmativeGoing: [goingToCES],
+    afirmativeNotGoing: [notGoingToCES],
+  }
+})
 }
