08268ba6523b11f79e1706b8eeaa04a76e9465b3
Jordan Jones <jordanalanjones@gmail.com>
2016-10-24 19:58:27 -0500

    Fixing hello message only being sent once

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -71,23 +71,23 @@
 'use strict'
 
 exports.handle = function handle(client) {
-  const sayHello = client.createStep({
-    satisfied() {
-      return Boolean(client.getConversationState().helloSent)
-    },
-
-    prompt() {
-      client.addResponse('app:response:name:welcome')
-      client.addResponse('app:response:name:provide/documentation', {
-        documentation_link: 'http://docs.init.ai',
-      })
-      client.addResponse('app:response:name:provide/instructions')
-      client.updateConversationState({
-        helloSent: true
-      })
-      client.done()
-    }
-  })
+  // const sayHello = client.createStep({
+  //   satisfied() {
+  //     return Boolean(client.getConversationState().helloSent)
+  //   },
+  //
+  //   prompt() {
+  //     client.addResponse('app:response:name:welcome')
+  //     client.addResponse('app:response:name:provide/documentation', {
+  //       documentation_link: 'http://docs.init.ai',
+  //     })
+  //     client.addResponse('app:response:name:provide/instructions')
+  //     client.updateConversationState({
+  //       helloSent: true
+  //     })
+  //     client.done()
+  //   }
+  // })
 
   const untrained = client.createStep({
     satisfied() {
